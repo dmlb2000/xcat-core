7647283cc4bc2da7ae822e4530f34ae9d0afd7de
jbjohnso <jbjohnso@35838ccf-6b3d-0410-a007-ed7a3c9b06b9>
2009-06-25 20:08:41 +0000

    -Fix breakage of normal nodeset actions
    git-svn-id: http://xcat.svn.sourceforge.net/svnroot/xcat/xcat-core/branches/2.2@3647 35838ccf-6b3d-0410-a007-ed7a3c9b06b9

--- xCAT-server/lib/xcat/plugins/destiny.pm
+++ xCAT-server/lib/xcat/plugins/destiny.pm
@@ -121,7 +121,7 @@ sub setdestiny {
       if ($ient->{kcmdline}) { $hash->{kcmdline} = $ient->{kcmdline} }
       $bptab->setNodeAttribs($_,$hash);
      }
-  } elsif ($state =~ /^install[=\$]/ or $state =~ /^netboot[=\$]/ or $state eq "image" or $state eq "winshell") {
+  } elsif ($state =~ /^install[=\$]/ or $state eq 'install' or $state =~ /^netboot[=\$]/ or $state eq 'netboot' or $state eq "image" or $state eq "winshell") {
     chomp($state);
     my $target;
     if ($state =~ /=/) {
