171b03c98953dbd1975365d979c18373a5468003
jbjohnso <jbjohnso@35838ccf-6b3d-0410-a007-ed7a3c9b06b9>
2010-11-16 15:51:52 +0000

    Backport guestostype vm table entry and skip reconfigs from xCAT tables
    git-svn-id: http://xcat.svn.sourceforge.net/svnroot/xcat/xcat-core/branches/2.5@8177 35838ccf-6b3d-0410-a007-ed7a3c9b06b9

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -181,7 +181,7 @@ vmmaster => {
     }
 },
 vm => {
-    cols => [qw(node host migrationdest storage storagemodel cfgstore memory cpus nics nicmodel bootorder clockoffset virtflags master vncport textconsole powerstate beacon datacenter comments disable)],
+    cols => [qw(node host migrationdest storage storagemodel cfgstore memory cpus nics nicmodel bootorder clockoffset virtflags master vncport textconsole powerstate beacon datacenter guestostype comments disable)],
     keys => [qw(node)],
     table_desc => 'Virtualization parameters',
     descriptions => {
@@ -210,6 +210,7 @@ vm => {
         'textconsole' => 'Tracks the Psuedo-TTY that maps to the serial port or console of a VM',
         'powerstate' => "This flag is used by xCAT to track the last known power state of the VM.",
         'beacon' => "This flag is used by xCAT to track the state of the identify LED with respect to the VM.",
+        'guestostype' => "This allows administrator to specify an identifier for OS to pass through to virtualization stack.  Normally this should be ignored as xCAT will translate from nodetype.os rather than requiring this field be used\n",
         'datacenter' => "Optionally specify a datacenter for the VM to exist in (only applicable to VMWare)",
     }
 },
@@ -787,6 +788,8 @@ site => {
    " tftpdir:  tftp directory path. Default is /tftpdir\n\n".
    " useSSHonAIX:  (yes/1 or no/0). If yes, ssh/scp will be setup and used. If no,\n".
    "               rsh/rcp will be setup and used on AIX. Default is yes.\n\n".
+   " vcenterautojoin: When set to no, the VMWare plugin will not attempt to auto remove and add hypervisors while trying to perform operations.  If users or tasks outside of xCAT perform the joining this assures xCAT will not interfere.\n\n".
+   " vmwarereconfigonpower: When set to no, the VMWare plugin will make no effort to push vm.cpus/vm.memory updates from xCAT to VMWare\n\n".
    " rsh:  This is no longer used. path to remote shell command for xdsh.\n\n".
    " rcp:  This is no longer used. path to remote copy command for xdcp.\n\n".
    " SNsyncfiledir:  The directory on the Service Node, where xdcp will copy the files\n".
